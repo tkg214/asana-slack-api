d8963bcebc8620e4420760468142070337601380
Ken Takagi <tkg214@gmail.com>
2017-05-04 11:41:54 -0700

    fixed bug with body-parser

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').load();
 
 const express       = require('express');
 const app           = express();
-const bodyParser    = ('body-parser');
+const bodyParser    = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
