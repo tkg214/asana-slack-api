37983099d0db503986f8c078b67e07cd6650561c
Ken Takagi <tkg214@gmail.com>
2017-05-04 14:57:52 -0700

    change url encoding body parser to parse json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express       = require('express');
 const app           = express();
 const bodyParser    = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const slackRoute = require('./routes/slack')();
 app.use('/slack', slackRoute);
