1be6defe0264da776d30b4f9ed95fd3697fa4d15
Ken Takagi <tkg214@gmail.com>
2017-05-04 13:41:38 -0700

    add get route for channel messages

--- routes/slack.js
+++ routes/slack.js
@@ -3,7 +3,12 @@ const slackRoute = require('express').Router();
 module.exports = () => {
 
   slackRoute.post('/channel-messages', (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
+    res.sendStatus(201);
+  });
+
+  slackRoute.get('/channel-messages', (req, res) => {
+    res.sendStatus(200);
   });
 
   return slackRoute;
